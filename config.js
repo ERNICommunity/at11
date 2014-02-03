27181cc56205a0ac16eeb9e8a4003b7df0b67869
Ján Neščivera <jan.nescivera@gmail.com>
2014-02-03 23:20:07 +0100

    introduced configurable restaurant repo, added application init logic

--- config.js
+++ config.js
@@ -1,3 +1,11 @@
 module.exports = {
-	port: 54321
+    port: 54321,
+    restaurants: [
+        {name: "U Daňováka", url: "http://www.obedovat.sk/restauracia/6801-u-danovaka/denne-menu", module: "obedovat"},
+        {name: "Giuliano", url: "http://www.giuliano.sk/sk/denne-menu/", module: "giuliano"},
+        {name: "ITB", url: "http://itbfood.sk/index.php?id=1&type=main_menu&t=1", module: "itb"},
+        {name: "Alfa", url: "http://www.obedovat.sk/restauracia/150-alfa/denne-menu", module: "obedovat"},
+        {name: "Škôlka", url: "http://jedalen.vysnivany.sk/ukazka-strany", module: "skolka"},
+        {name: "Pizza Pazza", url: "http://www.obedovat.sk/restauracia/8845-pizza-pazza-vienna-gate/denne-menu", module: "obedovat"}
+    ]
 };
