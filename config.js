891cb1660dd9155f9cd458f26c5939ec3359bd48
Ján Neščivera <jan.nescivera@gmail.com>
2014-02-19 19:56:05 +0100

    resolved #17

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ module.exports = {
         {name: "ITB", url: "http://itbfood.sk/index.php?id=1&type=main_menu&t=1", module: "itb"},
         {name: "Alfa", url: "http://www.obedovat.sk/restauracia/150-alfa/denne-menu", module: "obedovat"},
         {name: "Škôlka", url: "http://jedalen.vysnivany.sk/ukazka-strany", module: "skolka"},
-        {name: "Pizza Pazza", url: "http://www.obedovat.sk/restauracia/8845-pizza-pazza-vienna-gate/denne-menu", module: "obedovat"}
+        {name: "Pizza Pazza", url: "http://www.pizzapazza.sk/sk/denne-menu/", module: "pizzapazza"}
     ]
 };
