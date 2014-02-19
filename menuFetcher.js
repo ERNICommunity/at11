abbdca5f3daa906799f71d84650ca1e82a595fb6
Ján Neščivera <jan.nescivera@gmail.com>
2014-02-19 20:25:10 +0100

    files reformated

--- menuFetcher.js
+++ menuFetcher.js
@@ -6,37 +6,38 @@ module.exports = new (function() {
 		var menuObj = cache.get(url);
 		if (menuObj)
 			doneCallback({name: name, url: url, menu: menuObj});
-        else
-			load(url, parseCallback, function(menuObj){
-                cache.set(url, menuObj);
-                doneCallback({name: name, url: url, menu: menuObj});
-            });
+		else
+			load(url, parseCallback, function(menuObj) {
+				cache.set(url, menuObj);
+				doneCallback({name: name, url: url, menu: menuObj});
+			});
 	};
 })();
 
 function load(url, parseCallback, doneCallback) {
 	request(url, function(error, response, body) {
-        var menu;
-		if (!error && response.statusCode === 200) {
-            try
-            {
-                menu = parseCallback(body);
-                if(!Array.isArray(menu))
-                    throw "Invalid menu returned (expected array, got " + typeof menu + ")";
-            }
-            catch(e)
-            {
-                menu = ["Parser error", e];
-            }         
+		var menu;
+		if (!error && response.statusCode === 200)
+		{
+			try
+			{
+				menu = parseCallback(body);
+				if (!Array.isArray(menu))
+					throw "Invalid menu returned (expected array, got " + typeof menu + ")";
+			}
+			catch (e)
+			{
+				menu = ["Parser error", e];
+			}
 		}
-        else
-        {
-            menu = new Array();
-            if(error)
-                menu.push(error);
-            if (response)
-                menu.push("StatusCode: " + response.statusCode);
-        }
-        doneCallback(menu);
+		else
+		{
+			menu = new Array();
+			if (error)
+				menu.push(error);
+			if (response)
+				menu.push("StatusCode: " + response.statusCode);
+		}
+		doneCallback(menu);
 	});
 }
