201c47dae5fbb4c015b4b1e5bb4b5e32bc4ceb9d
Ján Neščivera <jan.nescivera@gmail.com>
2014-02-20 09:52:07 +0100

    closes #19

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     restaurants: [
         {name: "U Daňováka", url: "http://www.obedovat.sk/restauracia/6801-u-danovaka/denne-menu", module: "obedovat"},
         {name: "Giuliano", url: "http://www.giuliano.sk/sk/denne-menu/", module: "giuliano"},
-        {name: "ITB", url: "http://itbfood.sk/index.php?id=1&type=main_menu&t=1", module: "itb"},
+        {name: "ITB", url: "http://www.obedovat.sk/restauracia/1047-jedalen-7-plus/denne-menu", module: "obedovat"},
         {name: "Alfa", url: "http://www.obedovat.sk/restauracia/150-alfa/denne-menu", module: "obedovat"},
         {name: "Škôlka", url: "http://jedalen.vysnivany.sk/ukazka-strany", module: "skolka"},
         {name: "Pizza Pazza", url: "http://www.pizzapazza.sk/sk/denne-menu/", module: "pizzapazza"}
