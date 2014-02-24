9a5566bd8dbed26cb872e51bb734864cbfc13d6a
Ján Neščivera <jan.nescivera@gmail.com>
2014-02-24 22:27:42 +0100

    skolka parser improved

--- parsers/skolka.js
+++ parsers/skolka.js
@@ -1,30 +1,34 @@
 var cheerio = require('cheerio');
 
 module.exports = new (function() {
-    this.parse = function(html) {
+	this.parse = function(html) {
 
-        var $ = cheerio.load(html);
-        
-        var menu = new Array();
+		var $ = cheerio.load(html);
 
-        $('div.entry-content', '#post-2').children("p").each(function(){
-            var now = new Date();
-            var todayStr = now.getDate() + "." + (now.getMonth() + 1) + "." + now.getFullYear();
-            if(this.text().indexOf(todayStr) !== -1)
-            {          
-                menu.push(normalize(this.prev().contents().eq(1).text()));
-                menu.push(normalize(this.text().replace(todayStr, "")));
-                menu.push(normalize(this.next().text()));
-                return false;
-            }
-        });
-        
-        return menu;
+		var menu = new Array();
 
-        function normalize(str) {
-            return str.trim()
-                    .replace(/\s\s+/g, ' ')
-                    .replace(/^\d\.\s*/, '');
-        }
-    };
+		var lines = $('div.entry-content', '#post-2').text().split("\n").filter(function(val) {
+			return !/^\s*$/.test(val);
+		});
+		for (var i = 0; i < lines.length; i++)
+		{
+			var now = new Date();
+			var todayReg = new RegExp("^\\s*0?" + now.getDate() + "\\.\\s*0?" + (now.getMonth() + 1) + "\\.\\s*" + now.getFullYear());
+			if (todayReg.test(lines[i]))
+			{
+				menu.push(normalize(lines[i - 1].replace(/^\s*\w+\s+/, "")));
+				menu.push(normalize(lines[i].replace(todayReg, "")));
+				menu.push(normalize(lines[i + 1]));
+				break;
+			}
+		}
+
+		return menu;
+
+		function normalize(str) {
+			return str.trim()
+				.replace(/\s\s+/g, ' ')
+				.replace(/^\d\.\s*/, '');
+		}
+	};
 })();
