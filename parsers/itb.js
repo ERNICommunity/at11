3be2caee561698390ca184fddf9712a6b5630ef4
Ján Neščivera <jan.nescivera@gmail.com>
2014-02-25 19:57:11 +0100

    more robust ITB parser

--- parsers/itb.js
+++ parsers/itb.js
@@ -1,39 +1,44 @@
 var cheerio = require('cheerio');
 
 module.exports = new (function() {
-    this.parse = function(html) {
+	this.parse = function(html) {
 
-        var $ = cheerio.load(html);
+		var $ = cheerio.load(html);
 
-        var menu = new Array();
-        
-        $('td.cnt', '#contentBox').children('table').each(function(index) {
-            if(index === new Date().getDay() - 1)
-            {
-                menu = parseMenu(this, this.prev());
-                return false;
-            }
-        });
+		var menu = new Array();
 
-        return menu;
+		$('td.cnt', '#contentBox').children('table').each(function(index) {
+			if (index === new Date().getDay() - 1)
+			{
+				menu = parseMenu(this);
+				return false;
+			}
+		});
 
-        function parseMenu(table, p) {
-            var temp = [];
-            table.find('tr').each(function() {
-                temp.push(normalize(this.text().replace(/.\)/, '')));
-            });
-            var m = /Polievk.*:(.+)Špec.*:(.+)delená.*:(.+)$/ig.exec(p.text());
-            temp.unshift(normalize(m[3]));
-            temp.unshift(normalize(m[2]));
-            temp.unshift(normalize(m[1]));
-            return temp;
-        }
+		return menu;
 
-        function normalize(str) {
-            return str.trim()
-                    .replace(/\s\s+/g, ' ')
-                    .replace(/^\d\.\s*/, '')
-					.toLowerCase();
-        }
-    };
+		function parseMenu(table) {
+			var temp = [];
+			table.find('tr').each(function() {
+				temp.push(normalize(this.text().replace(/\w\)/, '')));
+			});
+			var txt = "";
+			table.prevUntil('table').each(function() {
+				txt = this.text() + txt;
+			});
+			var m = /Polievk.*:(.+)Špec.*:(.+)delená.*:(.+)$/ig.exec(txt);
+			for (var i = m.length - 1; i > 0; i--)
+			{
+				temp.unshift(normalize(m[i]));
+			}
+			return temp;
+		}
+
+		function normalize(str) {
+			return str.trim()
+				.replace(/\s\s+/g, ' ')
+				.replace(/^\d\.\s*/, '')
+				.toLowerCase();
+		}
+	};
 })();
