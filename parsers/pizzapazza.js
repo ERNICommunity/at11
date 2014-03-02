07af6ac494f3a22c4eb443d4b00ab1647bfef6f1
Ján Neščivera <jan.nescivera@gmail.com>
2014-03-02 17:34:04 +0100

    minor parser improvements

--- parsers/pizzapazza.js
+++ parsers/pizzapazza.js
@@ -7,22 +7,17 @@ module.exports = new (function() {
 
 		var menu = new Array();
 
-		var dayIndex = 0;
-		$('.editor-content > div:nth-child(2) > table:nth-child(1) > tbody').children().each(function() {
-			if (this.text().indexOf("Polievka") !== -1) {
-				dayIndex++;
-			}
-			if (dayIndex === new Date().getDay())
-			{
+		var days = ['nedeľa', 'pondelok', 'utorok', 'streda', 'štvrtok', 'piatok', 'sobota'];
+		$('.editor-content > div:nth-child(2) > table:nth-child(1) > tbody').children('tr').each(function() {
+			if (new RegExp(days[new Date().getDay()], 'i').test(this.text())) {
 				menu = menu.concat(parseDaily(this));
-				dayIndex = Number.POSITIVE_INFINITY;
 			}
-			if (this.text().indexOf('Týždenné menu') !== -1)
-				menu = menu.concat(parseWeekly(this));
-			if (this.text().indexOf('Šalátové menu') !== -1)
-				menu = menu.concat(parseSalads(this));
-			if (this.text().indexOf('Pizza menu') !== -1)
-				menu = menu.concat(parsePizza(this));
+			if (this.text().indexOf('VIP menu') !== -1 ||
+				this.text().indexOf('Týždenné menu') !== -1)
+				menu = menu.concat(parseOne(this));
+			if (this.text().indexOf('Šalátové menu') !== -1 ||
+				this.text().indexOf('Pizza menu') !== -1)
+				menu = menu.concat(parseThree(this));
 			if (this.children('th').length > 0 && this.text().indexOf("Hron") !== -1)//too far
 				return false;
 		});
@@ -31,26 +26,19 @@ module.exports = new (function() {
 
 		function parseDaily(elem) {
 			var arr = new Array();
-			arr.push(elem.children().first().contents().eq(2).text());
+			arr.push(elem.text().replace(/^[\s\S]+polievka:\s+/i, ''));
 			arr.push(normalize(elem.next().text()));
 			arr.push(normalize(elem.next().next().text()));
 			return arr;
 		}
 
-		function parseWeekly(elem) {
+		function parseOne(elem) {
 			var arr = new Array();
 			arr.push(normalize(elem.next().text()));
 			return arr;
 		}
 
-		function parseSalads(elem) {
-			var arr = new Array();
-			arr.push(normalize(elem.next().text()));
-			arr.push(normalize(elem.next().next().text()));
-			arr.push(normalize(elem.next().next().next().text()));
-			return arr;
-		}
-		function parsePizza(elem) {
+		function parseThree(elem) {
 			var arr = new Array();
 			arr.push(normalize(elem.next().text()));
 			arr.push(normalize(elem.next().next().text()));
