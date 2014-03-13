4b3379b910819144fddf78e693571c7df291ee79
Igor Kvasnicka <igor.kvasnicka@erni.sk>
2014-03-13 08:57:41 +0100

    soup is specially marked

--- parsers/skolka.js
+++ parsers/skolka.js
@@ -23,6 +23,11 @@ module.exports = new (function() {
 			}
 		}
 
+        //I think it is safe enough to assume that the first item in menu is the soup
+        if (menu.length > 0){
+            menu[0] = "<div class=\"soup\">" + menu[0] + "</div>";
+        }
+
 		return menu;
 
 		function normalize(str) {
