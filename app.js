7370494b41af3411852a32e2a69128ff08fda903
IgorKvasn <ikvaso@gmail.com>
2014-03-16 17:59:51 +0100

    login via passport module (no registration not DB, yet - users are hardcoded)

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+var flash = require('connect-flash')
 var express = require('express');
 var hbs = require('hbs');
-var urlModule = require('url')
+var urlModule = require('url');
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 
 //our modules
 var config = require('./config');
@@ -39,23 +44,127 @@ if(actions.length === 0)
 
 console.log("Initialization successful (" + actions.length + " of " + config.restaurants.length + ")");
 
+
+var users = [
+    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
+    , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+];
+
+function findById(id, fn) {
+    var idx = id - 1;
+    if (users[idx]) {
+        fn(null, users[idx]);
+    } else {
+        fn(new Error('User ' + id + ' does not exist'));
+    }
+}
+
+function findByUsername(username, fn) {
+    for (var i = 0, len = users.length; i < len; i++) {
+        var user = users[i];
+        if (user.username === username) {
+            return fn(null, user);
+        }
+    }
+    return fn(null, null);
+}
+
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+
+// Use the LocalStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a username and password), and invoke a callback
+//   with a user object.  In the real world, this would query a database;
+//   however, in this example we are using a baked-in set of users.
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        // asynchronous verification, for effect...
+        process.nextTick(function () {
+
+            // Find the user by username.  If there is no user with the given
+            // username, or the password is not correct, set the user to `false` to
+            // indicate failure and set a flash message.  Otherwise, return the
+            // authenticated `user`.
+            findByUsername(username, function(err, user) {
+                if (err) { return done(err); }
+                if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+                if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+                return done(null, user);
+            })
+        });
+    }
+));
+
+
 var app = express();
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
 app.use(express.static('static'));
-app.get('/', function(req, res) {
+app.configure(function() {
+//    app.use(express.logger());
+    app.use(express.cookieParser("thissecretrocks"));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride()); // must come after bodyParser
+    app.use(express.session({ secret: 'thissecretrocks', cookie: { maxAge: 60000 } }));
+    app.use(flash());
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(app.router);
+});
+
+app.get('/',  function(req, res) {
 	loadRestaurants(function(restaurants){
         res.setHeader('Content-Type', 'text/html; charset=UTF-8');
         res.setHeader('Content-Language', 'sk');
+
+        var parsedUrl = urlModule.parse(req.url, true).query;
+        if (parsedUrl.action){
+            res.render("login");
+            return;
+        }
+
         var now = new Date();
         var date = now.getDate() + ". " + (now.getMonth() + 1) + ". " + now.getFullYear();
 
         var theme = parseTheme(req);
 
+        if (req.user && req.user.username){
+            console.log("auth ok="+req.user.username);
+        }else{
+            console.log("auth fail");
+        }
+
         res.setHeader("Set-Cookie", ["theme="+ theme]);
         res.render(theme, { date: date, restaurants: restaurants});
+
     });
 });
+
+app.get('/loginerror',  function(req, res) {
+    res.redirect("/?action=auth");
+});
+
+app.post('/login',
+    passport.authenticate('local', { failureRedirect: '/loginerror', failureFlash: true }),
+    function(req, res) {
+        res.redirect('/');
+    });
+
 app.listen(config.port);
 console.log('Listening on port ' + config.port + '...');
 
