76df054fb39f11438b96ed6a09bbb2fcf5640a5a
Matúš Brliť <matus.brlit@gmail.com>
2014-04-18 21:59:32 +0200

    cheerio breaking change fixed
    fixes #32
    Cheerio v 0.14.0 - Fix passing context as DOM node
    set Date globally so it can be mocked
    switch for disabling cache

--- parsers/itb.js
+++ parsers/itb.js
@@ -1,48 +1,46 @@
 var cheerio = require('cheerio');
 
 module.exports = new (function() {
-	this.parse = function(html) {
+    this.parse = function(html) {
 
-		var $ = cheerio.load(html);
+        var $ = cheerio.load(html);
 
-		var menu = new Array();
+        var menu = new Array();
 
-		$('td.cnt', '#contentBox').children('table').each(function(index) {
-			if (index === new Date().getDay() - 1)
-			{
-				menu = parseMenu(this);
-				return false;
-			}
-		});
+        $('td.cnt', '#contentBox').children('table').each(function(index) {
+            if (index === new Date().getDay() - 1) {
+                menu = parseMenu(this);
+                return false;
+            }
+        });
 
-		return menu;
+        return menu;
 
-		function parseMenu(table) {
-			var temp = [];
-			table.find('tr').each(function() {
-				temp.push(normalize(this.text().replace(/\w\)/, '')));
-			});
-			var txt = "";
-			table.prevUntil('table').each(function() {
-				txt = this.text() + txt;
-			});
+        function parseMenu(table) {
+            var temp = [];
+            table.find('tr').each(function() {
+                temp.push(normalize(this.text().replace(/\w\)/, '')));
+            });
+            var txt = "";
+            table.prevUntil('table').each(function() {
+                txt = this.text() + txt;
+            });
 
             //format the soup
             txt = txt.replace(/Polievk.*:(.+)Špec.*:(.+)delená.*:(.+)$/ig, 'Polievk.*:<div class="soup">$1</div>Špec.*:$2delená.*:$3')
 
-			var m = /Polievk.*:(.+)Špec.*:(.+)delená.*:(.+)$/ig.exec(txt);
-			for (var i = m.length - 1; i > 0; i--)
-			{
-				temp.unshift(normalize(m[i]));
-			}
-			return temp;
-		}
+            var m = /Polievk.*:(.+)Špec.*:(.+)delená.*:(.+)$/ig.exec(txt);
+            for (var i = m.length - 1; i > 0; i--) {
+                temp.unshift(normalize(m[i]));
+            }
+            return temp;
+        }
 
-		function normalize(str) {
-			return str.trim()
+        function normalize(str) {
+            return str.trim()
 				.replace(/\s\s+/g, ' ')
 				.replace(/^\d\.\s*/, '')
 				.toLowerCase();
-		}
-	};
+        }
+    };
 })();
