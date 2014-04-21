73e97d05d9e80201f22b09bc6ec175ad4f9aff7c
Ján Neščivera <jan.nescivera@gmail.com>
2014-04-21 19:42:43 +0200

    improved ITB parser to not relay on day indexes

--- parsers/itb.js
+++ parsers/itb.js
@@ -1,4 +1,5 @@
 var cheerio = require('cheerio');
+var parserUtil = require('./parserUtil');
 
 module.exports = new (function() {
     this.parse = function(html) {
@@ -6,26 +7,27 @@ module.exports = new (function() {
         var $ = cheerio.load(html);
 
         var menu = new Array();
+        var todayNameRegex = new RegExp("^\\s*" + parserUtil.dayNameMap[global.todaysDate.getDay()], "i");
 
-        $('td.cnt', '#contentBox').children('table').each(function(index) {
-            if (index === global.todaysDate.getDay() - 1) {
-                menu = parseMenu($(this));
+        $('td.cnt', '#contentBox').children('table').each(function() {
+            var precedingText = "";
+            $(this).prevUntil('table').each(function() {
+                precedingText = $(this).text() + precedingText;
+            });
+            if (todayNameRegex.test(precedingText)) {
+                menu = parseMenu($(this), precedingText);
                 return false;
             }
         });
 
         return menu;
 
-        function parseMenu(table) {
+        function parseMenu(table, mixedText) {
             var temp = [];
             table.find('tr').each(function() {
                 temp.push({isSoup: false, text: normalize($(this).text())});
             });
             
-            var mixedText = "";
-            table.prevUntil('table').each(function() {
-                mixedText = $(this).text() + mixedText;
-            });
             var m = /Polievk.*:(.+)Špec.*:(.+)delená.*:(.+)$/i.exec(mixedText);
             for (var i = m.length - 1; i > 0; i--) {
                 temp.unshift({isSoup: false, text: normalize(m[i])});
