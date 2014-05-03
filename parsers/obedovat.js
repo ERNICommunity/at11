845a6df6495654cc651703d088dffe1faec0ef1c
Matúš Brliť <matus.brlit@gmail.com>
2014-05-03 20:47:43 +0200

    Ergenau weekend menu parsing

--- parsers/obedovat.js
+++ parsers/obedovat.js
@@ -1,20 +1,30 @@
 var cheerio = require('cheerio');
 var parserUtil = require('./parserUtil');
 
-module.exports = new (function () {
-    this.parse = function (html) {
+module.exports = new (function() {
+    this.parse = function(html) {
 
         var $ = cheerio.load(html);
 
         var menu = new Array();
         var todayStr = global.todaysDate.format("D.M.YYYY");
 
-        $('.daily-menu-for-day').each(function () {
-            if ($(this).children("header").first().text().indexOf(todayStr) !== -1) {
+        $('.daily-menu-for-day').each(function() {
+            if ($(this).children("header").first().text().indexOf(todayStr) !== -1)
+            {
                 menu = parseMenu($(this));
                 //I think it is safe enough to assume that the first item in menu is the soup
-                if (menu.length > 0) {
-                    menu[0].isSoup = true;
+                //for Ergenau on weekends, the first item is "Vikendove menu"
+                if (menu.length > 0)
+                {
+                    if (/menu/.test(menu[0].text))
+                    {
+                        menu[1].isSoup = true;
+                        menu[1].price = menu[0].price;
+                        menu.splice(0, 1);
+                    }
+                    else
+                    { menu[0].isSoup = true; }
                 }
                 return false;
             }
@@ -26,15 +36,16 @@ module.exports = new (function () {
             var arr = new Array();
 
             var header = normalize(elem.find('header.rm').first().text());
-            if (header && !endsWith(header, 'menu')) {
-                arr.push({isSoup: false, text: header, price: NaN});
+            if (header && !endsWith(header, 'menu'))
+            {
+                arr.push({ isSoup: false, text: header, price: NaN });
             }
 
-            elem.find('li').each(function () {
+            elem.find('li').each(function() {
                 var text = normalize($(this).children(".name").first().text());
                 var priceMatch = /(\d+[\.,]\d+) ?€/.exec($(this).children(".price").first().text().replace(",", "."));
                 var price = priceMatch ? parseFloat(priceMatch[1]) : NaN;
-                arr.push({isSoup: false, text: text, price: price});
+                arr.push({ isSoup: false, text: text, price: price });
             });
             return arr;
         }
