c1032a822fdc8b0f2fa2260ee34f03ff387de8ae
Ján Neščivera <jan.nescivera@gmail.com>
2014-05-08 14:47:05 +0200

    fix for icons not being displayed in IE

--- static/js/script.js
+++ static/js/script.js
@@ -10,18 +10,25 @@ $(document).ready(function() {
                 var ul = $("<ul></ul>");
                 if($.isEmptyObject(data.menu))
                 {
-                    ul.append("<li class=\"error\"><span>Nepodarilo sa načítať menu, skús pozrieť priamo na stránke reštaurácie</span></li>");
+                    ul.append("<li class=\"error\"><span>\uf06a</span><span>Nepodarilo sa načítať menu, skús pozrieť priamo na stránke reštaurácie</span></li>");
                 }
                 else
                 {
                     data.menu.forEach(function(item){
                         var li = $("<li></li>");
-                        if(item.isSoup)
+                        if(item.isSoup) {
                             li.addClass("soup");
-                        if(item.isError)
+                            li.append("<span>\uf0f4</span>");
+                        }
+                        else if(item.isError){
                             li.addClass("error");
+                            li.append("<span>\uf06a</span>");
+                        }
+                        else 
+                            li.append("<span>\uf0f5</span>");
                         li.append("<span>" + item.text + "</span>");
-                        li.append("<span class=\"price\">" + item.price + "</span>");
+                        if(item.price)
+                            li.append("<span class=\"price\">" + item.price + "</span>");
                         ul.append(li);
                     });
                 }
@@ -29,7 +36,7 @@ $(document).ready(function() {
                 section.append("<i>" + data.timeago + "</i>");
             })
             .fail(function(jqXHR, textStatus, errorThrown) {
-                section.append("<ul><li class=\"error\"><span>" + textStatus + "</span></li></ul>");
+                section.append("<ul><li class=\"error\"><span>\uf06a</span><span>" + textStatus + "</span></li></ul>");
             })
             .always(function(){
                 section.find(".fa-spin").remove();
