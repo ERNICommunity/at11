977508f10bdbd8d9e29854e9cb8557fa2197db21
Jakub Šturc <jakub.sturc@gmail.com>
2014-05-09 09:25:07 +0200

    Update themes.js
    Related to #56. Fixing themes authors. This still doesn't fix the naming issue.

--- themes.js
+++ themes.js
@@ -1,8 +1,8 @@
 module.exports = {
     themes:
         {
-            1: { id: 1, name: "Jakub", template: "../views/index.html" },
-            2: { id: 2, name: "Jano", template: "../views/index2.html" },
+            1: { id: 1, name: "Jano", template: "../views/index.html" },
+            2: { id: 2, name: "Igor", template: "../views/index2.html" },
             3: { id: 3, name: "Matúš", template: "../views/index3.html" }
         }
 };
