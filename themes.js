10817627aaee71581ea9912623064ccdb98b39db
Matúš Brliť <matus.brlit@gmail.com>
2014-05-09 08:59:47 +0200

    Name the themes
    fixes #56 :)

--- themes.js
+++ themes.js
@@ -1,8 +1,8 @@
 module.exports = {
     themes:
         {
-            1: { id: 1, name: "Theme 1", template: "../views/index.html" },
-            2: { id: 2, name: "Theme 2", template: "../views/index2.html" },
-            3: { id: 3, name: "Theme 3", template: "../views/index3.html" }
+            1: { id: 1, name: "Jakub", template: "../views/index.html" },
+            2: { id: 2, name: "Jano", template: "../views/index2.html" },
+            3: { id: 3, name: "Matúš", template: "../views/index3.html" }
         }
 };
