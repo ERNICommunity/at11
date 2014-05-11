550a9ffaadf7c479db0f4d690ea1c731a94d6b95
Ján Neščivera <jan.nescivera@gmail.com>
2014-05-11 10:47:47 +0200

    correctly handling callback errors

--- menuFetcher.js
+++ menuFetcher.js
@@ -36,25 +36,32 @@ function load(url, parseCallback, doneCallback) {
                     clearTimeout(timer);
                     timer = null;//clearTimeout does not null the value
 
-                    if (!Array.isArray(menuItems))
-                        throw "Invalid menu returned (expected array, got " + typeof menuItems + ")";
+                    try
+                    {
+                        if (!Array.isArray(menuItems))
+                            throw "Invalid menu returned (expected array, got " + typeof menuItems + ")";
 
-                    //check if each menu item has required attributes
-                    menuItems.forEach(function (item) {
-                        if (typeof item !== "object")
-                            throw "Each item should be object, but got " + typeof item;
-                        if (typeof item.isSoup !== "boolean")
-                            throw "Menu item has wrong 'isSoup' flag (" + typeof item.isSoup + ")";
-                        if (typeof item.text !== "string")
-                            throw "Menu item has wrong 'text' property (" + typeof item.text + ")";
-                        if (typeof item.price !== "number")
-                            throw "Menu item has wrong 'price' property (" + typeof item.price + ")";
-                        item.price = isNaN(item.price) ? "" : item.price.toFixed(2).replace(".", ",") + " €";//convert to presentable form
-                    });
-                    doneCallback(menuItems);
+                        //check if each menu item has required attributes
+                        menuItems.forEach(function (item) {
+                            if (typeof item !== "object")
+                                throw "Each item should be object, but got " + typeof item;
+                            if (typeof item.isSoup !== "boolean")
+                                throw "Menu item has wrong 'isSoup' flag (" + typeof item.isSoup + ")";
+                            if (typeof item.text !== "string")
+                                throw "Menu item has wrong 'text' property (" + typeof item.text + ")";
+                            if (typeof item.price !== "number")
+                                throw "Menu item has wrong 'price' property (" + typeof item.price + ")";
+                            item.price = isNaN(item.price) ? "" : item.price.toFixed(2).replace(".", ",") + " €";//convert to presentable form
+                        });
+                        doneCallback(menuItems);
+                    }
+                    catch(err)//catches callback errors
+                    {
+                        doneCallback([{ isError: true, text: err, price: "" }]);
+                    }
                 });
             }
-            catch (err)
+            catch (err)//catches only synchronous errors
             {
                 clearTimeout(timer);
                 timer = null;//clearTimeout does not null the value
