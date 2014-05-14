3e8ee833e62fe2a0e4c5a11c5f9a125c061a2289
Ján Neščivera <jan.nescivera@gmail.com>
2014-05-14 20:00:11 +0200

    kamnica tests added

--- test/kamenica.js
+++ test/kamenica.js
@@ -53,4 +53,52 @@ describe('kamenica', function() {
             assert.equal(isNaN(menu[4].price), true);
         });
     });
+    
+    describe('parsing sample 2014-05-14', function() {
+
+        var html = fs.readFileSync(__dirname + '/samples/Kamenica.2014-05-14.html', { encoding: "utf-8" });
+        var menu;
+
+        before(function(done) {
+            global.todaysDate = moment("2014-05-14");
+            sme.parse(html, function(menuItems) {
+                menu = menuItems;
+                done();
+            });
+        });
+
+        it("should return 5 items", function() {
+            assert.equal(menu.length, 5);
+        });
+
+        it("1st item correct", function() {
+            assert.equal(menu[0].isSoup, true);
+            assert.equal(menu[0].text, "Zemiaková kyslá");
+            assert.equal(isNaN(menu[0].price), true);
+        });
+
+        it("2nd item correct", function() {
+            assert.equal(menu[1].isSoup, true);
+            assert.equal(menu[1].text.trim(), "Paradajková");
+            assert.equal(isNaN(menu[1].price), true);
+        });
+
+        it("3rd item correct", function() {
+            assert.equal(menu[2].isSoup, false);
+            assert.equal(menu[2].text, "Kurací gyros, ½ ryža ½ hranolky");
+            assert.equal(isNaN(menu[2].price), true);
+        });
+
+        it("4th item correct", function() {
+            assert.equal(menu[3].isSoup, false);
+            assert.equal(menu[3].text, "Vyprážaný bravčový rezeň, zemiaková kaša, kyslá uhorka");
+            assert.equal(isNaN(menu[3].price), true);
+        });
+
+        it("5th item correct", function() {
+            assert.equal(menu[4].isSoup, false);
+            assert.equal(menu[4].text, "Grilovaná zelenina, varené zemiaky, cesnakový dressing 300");
+            assert.equal(isNaN(menu[4].price), true);
+        });
+    });
 });
