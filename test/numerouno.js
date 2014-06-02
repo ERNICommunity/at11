beec51250b4e553c65e57a7d17cb8f65d44929ab
Ján Neščivera <jan.nescivera@gmail.com>
2014-06-02 22:31:59 +0200

    replaced test of old format with new one, covers #68

--- test/numerouno.js
+++ test/numerouno.js
@@ -6,61 +6,13 @@ var assert = require('assert'),
 moment.lang('sk');
 
 describe('numerouno', function() {
-    describe('parsing sample 2014-04-29', function() {
+    describe('parsing sample 2014-06-02', function() {
 
-        var html = fs.readFileSync(__dirname + '/samples/NumeroUno.2014-04-29.html', {encoding:"utf-8"});
+        var html = fs.readFileSync(__dirname + '/samples/NumeroUno.2014-06-02.html', {encoding:"utf-8"});
         var menu;
 
         before(function(done) {
-            global.todaysDate = moment("2014-04-29");
-            parser.parse(html, function(menuItems) {
-                menu = menuItems;
-                done();
-            });
-        });
-
-        it("should return 5 items", function() {
-            assert.equal(menu.length, 5);
-        });
-        
-        it("1st item correct", function() {
-            assert.equal(menu[0].isSoup, true);
-            assert.equal(menu[0].text, "Fazuľová polievka");
-            assert.equal(isNaN(menu[0].price), true);
-        });
-        
-        it("2nd item correct", function() {
-            assert.equal(menu[1].isSoup, false);
-            assert.equal(menu[1].text.trim(), "Lasagne bolognesse – zapekané cestoviny s boloňskou omáčkou a so syrom");
-            assert.equal(menu[1].price, 3.90);
-        });
-        
-        it("3rd item correct", function() {
-            assert.equal(menu[2].isSoup, false);
-            assert.equal(menu[2].text, "Bryndzové halušky so slaninkou a so smotanou");
-            assert.equal(menu[2].price, 3.50);
-        });
-        
-        it("4th item correct", function() {
-            assert.equal(menu[3].isSoup, false);
-            assert.equal(menu[3].text, "Chicken Salat – vyprážané kuracie kúsky v cornflexoch/ grilované kuracie kúsky na veľkom zeleninovom šaláte");
-            assert.equal(menu[3].price, 4.90);
-        });
-        
-        it("5th item correct", function() {
-            assert.equal(menu[4].isSoup, false);
-            assert.equal(menu[4].text, "menu č. 4 po vypredaní menu č. 1 a 2 do 13:00 h: Vyprážaný syr s hranolkami, tatárska omáčka");
-            assert.equal(menu[4].price, 4.50);
-        });
-    });
-
-    describe('parsing sample 2014-05-02', function() {
-
-        var html = fs.readFileSync(__dirname + '/samples/NumeroUno.2014-05-02.html', {encoding:"utf-8"});
-        var menu;
-
-        before(function(done) {
-            global.todaysDate = moment("2014-05-02");
+            global.todaysDate = moment("2014-06-02");
             parser.parse(html, function(menuItems) {
                 menu = menuItems;
                 done();
@@ -73,26 +25,26 @@ describe('numerouno', function() {
         
         it("1st item correct", function() {
             assert.equal(menu[0].isSoup, true);
-            assert.equal(menu[0].text, "Hovädzí vývar s haluškami");
+            assert.equal(menu[0].text, "Šošovicová polievka");
             assert.equal(isNaN(menu[0].price), true);
         });
         
         it("2nd item correct", function() {
             assert.equal(menu[1].isSoup, false);
-            assert.equal(menu[1].text.trim(), "Vyprážaný kurací Cordon Bleu s opekanými zemiakmi, tatárska omáčka");
+            assert.equal(menu[1].text.trim(), "Grilovaný bravčový steak s volským okom, ½ ryža, ½ hranolky");
             assert.equal(menu[1].price, 3.90);
         });
         
         it("3rd item correct", function() {
             assert.equal(menu[2].isSoup, false);
-            assert.equal(menu[2].text, "Zeleninové rizoto so syrom, šalátik");
+            assert.equal(menu[2].text, "Sphagetti Aglio Olio – špagety s čili papričkami na olivovom oleji, bylinky");
             assert.equal(menu[2].price, 3.50);
         });
         
         it("4th item correct", function() {
             assert.equal(menu[3].isSoup, false);
-            assert.equal(menu[3].text, "menu č. 4 po vypredaní menu č. 1 a 2 do 13:00 h: Vyprážaný syr s hranolkami, tatárska omáčka");
-            assert.equal(menu[3].price, 4.50);
+            assert.equal(menu[3].text, "Chicken salat – veľký miešaný šalát s vyprážanými kuracími tyčinkami v sézame");
+            assert.equal(menu[3].price, 5.50);
         });
     });
 });
