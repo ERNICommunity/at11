b949a5b31cc9da5550f4b89b3e5e3abf0a930b0e
Ján Neščivera <jan.nescivera@gmail.com>
2014-06-02 22:39:33 +0200

    tests for #69

---
+++ test/skolka.js
@@ -0,0 +1,44 @@
+var assert = require('assert'),
+    fs = require('fs'),
+    moment = require('moment-timezone'),
+    parser = require('../parsers/skolka');
+
+moment.lang('sk');
+
+describe('skolka', function() {
+    describe('parsing sample 2014-06-02', function() {
+
+        var html = fs.readFileSync(__dirname + '/samples/Skolka.2014-06-02.html', { encoding: "utf-8" });
+        var menu;
+
+        before(function(done) {
+            global.todaysDate = moment("2014-06-02");
+            parser.parse(html, function(menuItems) {
+                menu = menuItems;
+                done();
+            });
+        });
+
+        it("should return 3 items", function() {
+            assert.equal(menu.length, 3);
+        });
+
+        it("1st item correct", function() {
+            assert.equal(menu[0].isSoup, true);
+            assert.equal(menu[0].text, "Rajčinová polievka");
+            assert.equal(isNaN(menu[0].price), true);
+        });
+
+        it("2nd item correct", function() {
+            assert.equal(menu[1].isSoup, false);
+            assert.equal(menu[1].text.trim(), "Kuracie prsia s nivovou omáčkou, ryža");
+            assert.equal(isNaN(menu[1].price), true);
+        });
+
+        it("3rd item correct", function() {
+            assert.equal(menu[2].isSoup, false);
+            assert.equal(menu[2].text, "Domáce buchty plnené džemom");
+            assert.equal(isNaN(menu[2].price), true);
+        });
+    });
+});
