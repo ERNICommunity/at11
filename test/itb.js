74d1fd28ac98e06c8a45aeca754449b2ab946e0a
Ján Neščivera <jan.nescivera@gmail.com>
2014-06-21 13:36:49 +0200

    tests for #71 and #73

--- test/itb.js
+++ test/itb.js
@@ -137,4 +137,70 @@ describe('itb', function() {
             assert.equal(menu[7].price, 3.20);
         });
     });
+    
+    describe('parsing sample 2014-06-11', function() {
+
+        var html = fs.readFileSync(__dirname + '/samples/ITB.2014-06-11.html', { encoding: "utf-8" });
+        var menu;
+
+        before(function(done) {
+            global.todaysDate = moment("2014-06-11");
+            parser.parse(html, function(menuItems) {
+                menu = menuItems;
+                done();
+            });
+        });
+
+        it("should return 8 items", function() {
+            assert.equal(menu.length, 8);
+        });
+
+        it("1st item correct", function() {
+            assert.equal(menu[0].isSoup, true);
+            assert.equal(menu[0].text, "Slepačia");
+            assert.equal(menu[0].price, 1.00);
+        });
+
+        it("2nd item correct", function() {
+            assert.equal(menu[1].isSoup, true);
+            assert.equal(menu[1].text.trim(), "Mrkvová");
+            assert.equal(menu[1].price, 0.90);
+        });
+
+        it("3rd item correct", function() {
+            assert.equal(menu[2].isSoup, false);
+            assert.equal(menu[2].text, "Grilované kurača, pečené zemiaky so zeleninou");
+            assert.equal(menu[2].price, 4.50);
+        });
+
+        it("4th item correct", function() {
+            assert.equal(menu[3].isSoup, false);
+            assert.equal(menu[3].text, "Pečený haik, pečená tekvica s brokolicou");
+            assert.equal(menu[3].price, 3.30);
+        });
+
+        it("5th item correct", function() {
+            assert.equal(menu[4].isSoup, false);
+            assert.equal(menu[4].text, "Vyprážané rybie filé, zemiaková kaša");
+            assert.equal(menu[4].price, 3.20);
+        });
+        
+        it("6th item correct", function() {
+            assert.equal(menu[5].isSoup, false);
+            assert.equal(menu[5].text, "Morčacie medajlónky na šafráne, dusená ryža");
+            assert.equal(menu[5].price, 3.20);
+        });
+        
+        it("7th item correct", function() {
+            assert.equal(menu[6].isSoup, false);
+            assert.equal(menu[6].text, "Jelenie ragú so šampiňónmi, halušky");
+            assert.equal(menu[6].price, 3.50);
+        });
+        
+        it("8th item correct", function() {
+            assert.equal(menu[7].isSoup, false);
+            assert.equal(menu[7].text, "Pečené huby na tymiane, zemiaková kaša");
+            assert.equal(menu[7].price, 3.20);
+        });
+    });
 });
