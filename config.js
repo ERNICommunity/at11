b589549e99e8643712cdb000fe464201d3167f74
Matúš Brliť <matus.brlit@gmail.com>
2014-08-06 22:02:15 +0200

    Added new theme

--- config.js
+++ config.js
@@ -18,6 +18,7 @@ module.exports = {
     themes: {
         "jano": { name: "Jano", template: "../views/index.html" },
         "igor": { name: "Igor", template: "../views/index2.html" },
-        "matus": { name: "Matúš", template: "../views/index3.html" }
+        "matus": { name: "Matúš", template: "../views/index3.html" },
+        "iveta": { name: "Iveta", template: "../views/index4.html" }
     }
 };
