7694ed6e99f6a9a100e5c1c564b3f2187bcff1e9
Jakub Šturc <jakub.sturc@gmail.com>
2014-09-21 21:36:24 +0200

    ... maybe last commit broke something.

--- config.js
+++ config.js
@@ -4,7 +4,6 @@ module.exports = {
     globalTickInterval: 2*60*1000, //2m
     parserTimeout: 20*1000, //20s 
     restaurants: [
-        {id: 0, name: "Jedáleň u Daňováka", url: "http://www.obedovat.sk/restauracia/9812-jedalenudanovaka2/denne-menu", module: "obedovat"},
         {id: 1, name: "U Daňováka", url: "http://www.obedovat.sk/restauracia/6801-u-danovaka/denne-menu", module: "obedovat"},
         {id: 2, name: "Giuliano", url: "http://www.giuliano.sk/sk/denne-menu/", module: "giuliano"},
         {id: 3, name: "ITB", url: "http://www.itbfood.sk/index.php?id=1&type=main_menu&t=1", module: "itb"},
@@ -14,7 +13,8 @@ module.exports = {
         {id: 7, name: "Kamenica - Corleone Pizza", url: "http://restauracie.sme.sk/restauracia/kamenica-corleone-pizza_660-petrzalka_664/denne-menu", module: "sme"},
         {id: 8, name: "Numero Uno", url: "http://www.numerouno.sk/denne-menu-5/", module: "numerouno"},
         {id: 9, name: "Engerau restaurant", url: "http://www.obedovat.sk/restauracia/241-engerau-restaurant/denne-menu", module: "obedovat"},
-        {id:10, name: "Jedáleň Albatros", url: "http://www.obedovat.sk/restauracia/7088-albatros-jedalen/denne-menu", module: "obedovat"}
+        {id:10, name: "Jedáleň Albatros", url: "http://www.obedovat.sk/restauracia/7088-albatros-jedalen/denne-menu", module: "obedovat"},
+        {id: 11, name: "Jedáleň u Daňováka", url: "http://www.obedovat.sk/restauracia/9812-jedalenudanovaka2/denne-menu", module: "obedovat"}
     ],
     themes: {
         "jano": { name: "Jano", template: "../views/index.html" },
