c2fa4fb1674b9739f9c0f28cef7fa4ac6e52ff21
Jakub Šturc <jakub.sturc@gmail.com>
2014-09-21 21:33:12 +0200

    Jedáleň u Daňováka II added

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ module.exports = {
     globalTickInterval: 2*60*1000, //2m
     parserTimeout: 20*1000, //20s 
     restaurants: [
+        {id: 0, name: "Jedáleň u Daňováka", url: "http://www.obedovat.sk/restauracia/9812-jedalenudanovaka2/denne-menu", module: "obedovat"},
         {id: 1, name: "U Daňováka", url: "http://www.obedovat.sk/restauracia/6801-u-danovaka/denne-menu", module: "obedovat"},
         {id: 2, name: "Giuliano", url: "http://www.giuliano.sk/sk/denne-menu/", module: "giuliano"},
         {id: 3, name: "ITB", url: "http://www.itbfood.sk/index.php?id=1&type=main_menu&t=1", module: "itb"},
