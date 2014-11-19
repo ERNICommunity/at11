f006ef1cff0a0da2be77eb1235966af2318e3f29
Jakub Šturc <jakub.sturc@gmail.com>
2014-11-19 11:45:08 +0100

    A new url for ITB

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
         {id: 0, name: "Jedáleň u Daňováka II", url: "http://www.obedovat.sk/restauracia/9812-jedalenudanovaka2/denne-menu", module: "obedovat"},
         {id: 1, name: "U Daňováka", url: "http://www.obedovat.sk/restauracia/6801-u-danovaka/denne-menu", module: "obedovat"},
         {id: 2, name: "Giuliano", url: "http://www.giuliano.sk/sk/denne-menu/", module: "giuliano"},
-        {id: 3, name: "ITB", url: "http://www.itbfood.sk/index.php?id=1&type=main_menu&t=1", module: "itb"},
+        {id: 3, name: "ITB", url: "http://www.citycantina.sk/index.php?id=1&type=main_menu&t=1", module: "itb"},
         {id: 4, name: "Alfa", url: "http://www.obedovat.sk/restauracia/150-alfa/denne-menu", module: "obedovat"},
         {id: 5, name: "Škôlka", url: "http://jedalen.vysnivany.sk/ukazka-strany", module: "skolka"},
         {id: 6, name: "Pizza Pazza", url: "http://www.pizzapazza.sk/sk/denne-menu/", module: "pizzapazza"},
