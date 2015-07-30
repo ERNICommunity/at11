8fe860d67b4ee57abe1b11b1bbe19ffbb6238d5a
Jakub Šturc <jakub.sturc@gmail.com>
2015-07-30 08:18:02 +0200

    Update engerau.js
    one more fix for a build.

--- parsers/engerau.js
+++ parsers/engerau.js
@@ -3,7 +3,7 @@ var zomato = require('./zomato');
 module.exports.parse = function(html, callback) {
   zomato.parse(html, function(menuItems) {
     for (var i = 0; i < menuItems.length; i++) {
-      menu = menuItems[i].menu;
+      var menu = menuItems[i].menu;
       var price = NaN;
       for (var j = 0; j < menu.length; j++) {
         var item = menu[j];
