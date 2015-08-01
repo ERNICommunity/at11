f4bff05f2e32bc44fff69c87f240a4ea5f5cbc50
Ján Neščivera <jan.nescivera@gmail.com>
2015-08-01 16:17:49 +0200

    improving  string prototype extensions

--- parsers/parserUtil.js
+++ parsers/parserUtil.js
@@ -15,32 +15,32 @@ module.exports.parsePrice = function(item) {
 
 var accentPairs = { 'a': "á", 'e': 'é', 'i': 'í', 'o': 'ó', 'u': 'ú', 'y': 'ý', 't': 'ť', 'l': 'ľ' };
 
-global.String.prototype.tidyAfterOCR = function() {
+global.String.prototype.tidyAfterOCR = global.String.prototype.tidyAfterOCR || function() {
     return this.replace(/(\w)[`']/g, function(m, g) {
         return accentPairs[g] || m;
     }).replace('%:', '€');
 };
 
-global.String.prototype.normalizeWhitespace = function() {
+global.String.prototype.normalizeWhitespace = global.String.prototype.normalizeWhitespace || function() {
     // also single spaces are replaced as there are different charcodes for space (32 vs. 160)
     // and we need to be consistent because of comparisons in tests
     return this.trim().replace(/\s+/g, ' ');
 };
 
-global.String.prototype.correctCommaSpacing = function() {
+global.String.prototype.correctCommaSpacing = global.String.prototype.correctCommaSpacing || function() {
     return this.replace(/(\S) *(,|\.) *(\S)/g, '$1$2 $3');
 };
 
-global.String.prototype.removeMetrics = function() {
+global.String.prototype.removeMetrics = global.String.prototype.removeMetrics || function() {
     //after metrics removal there might be whitespaces left at the ends so trim it afterwards
     return this.replace(/\s*\(?(?:\d+\/)?( ?\d[\doO\s]*)+ *(?:[,\.]\d[\doO]*)? *[lLgG]\)?\.?\s*/g, ' ').trim();
 };
 
-global.String.prototype.capitalizeFirstLetter = function() {
+global.String.prototype.capitalizeFirstLetter = global.String.prototype.capitalizeFirstLetter || function() {
     return this.replace(/(^[A-Za-z\u00C0-\u017F])/, function(a) { return a.toUpperCase(); });
 };
 
-global.String.prototype.removeItemNumbering = function() {
+global.String.prototype.removeItemNumbering = global.String.prototype.removeItemNumbering || function() {
     return this.trim().replace(/^\W\s+/, '').replace(/^[\w\d] *[\)\.,]+[AB]?\s*/, '').trim();
 };
 
