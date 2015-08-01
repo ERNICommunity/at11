7d27d5ab1e29b543582bb04ce89fc05e2e1f9a0f
Ján Neščivera <jan.nescivera@gmail.com>
2015-08-01 14:23:14 +0200

    moved loadMenus function into utils.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-/* global writeCookie, readCookie, CurrentDay */
+/* global writeCookie, readCookie, loadMenus */
 
 $(document).ready(function() {
     var container = $("#container");
@@ -38,52 +38,6 @@ $(document).ready(function() {
         $(this).toggleClass('hover-effect');
     });
 });
-
-function loadMenus(container) {
-    $("section", container).each(function() {
-        var section = $(this);
-        var restaurantId = section.data("restaurantId");
-        $.ajax("/menu/" + restaurantId + "/" + CurrentDay())
-                .done(function(data) {
-                    var ul = $("<ul></ul>");
-                    if ($.isEmptyObject(data.menu)) {
-                        ul.append("<li class='error'><i>\uf071</i><span>Nepodarilo sa načítať menu, skús pozrieť priamo na stránke reštaurácie</span></li>");
-                    }
-                    else {
-                        data.menu.forEach(function(item) {
-                            var li = $("<li></li>");
-                            if (item.isSoup) {
-                                li.addClass("soup");
-                                li.append("<i>\uf1b1</i>");
-                            }
-                            else if (item.isError) {
-                                li.addClass("error");
-                                li.append("<i>\uf071</i>");
-                            }
-                            else {
-                                li.append("<i>\uf0f5</i>");
-                            }
-                            li.append("<span>" + item.text + "</span>");
-                            if (item.price) {
-                                li.append("<span class='price'>" + item.price + "</span>");
-                            }
-                            ul.append(li);
-                        });
-                    }
-                    section.append(ul);
-                    if (data.timeago !== undefined) {
-                        section.append("<span class='timeago'><i class='fa fa-refresh'></i> " + data.timeago + "</span>");
-                    }
-                })
-                .fail(function(jqXHR, textStatus) {
-                    section.append("<ul><li class='error'><i>\uf071</i><span>" + textStatus + "</span></li></ul>");
-                })
-                .always(function() {
-                    section.find(".fa-spinner").remove();
-                    container.masonry();
-                });
-    });
-}
 
 function initialHide(container) {
     window.hiddenRestaurants = {};
