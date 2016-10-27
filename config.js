a8111ef2e531a6bd0971273ba9de089d5a2726fb
Ján Neščivera <jan.nescivera@gmail.com>
2016-10-27 18:53:48 +0200

    fixed zomato URLs

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
 	cacheExpiration: 2 * 60 * 60 * 1000, //2h
 	parserTimeout: 20 * 1000, //20s
 	restaurants: [
-	{ id: 1, name: 'U Daňováka', url: 'https://www.zomato.com/sk/bratislava/re%C5%A1taur%C3%A1cia-u-da%C5%88ov%C3%A1ka-petr%C5%BEalka-bratislava-v/menu#tabtop', module: 'zomato' },
+	{ id: 1, name: 'U Daňováka', url: 'https://www.zomato.com/sk/bratislava/re%C5%A1taur%C3%A1cia-u-da%C5%88ov%C3%A1ka-petr%C5%BEalka-bratislava-v/denn%C3%A9-menu', module: 'zomato' },
 	{ id: 2, name: 'Giuliano', url: 'http://www.giuliano.sk/sk/denne-menu/', module: 'giuliano' },
 	{ id: 3, name: 'ITB', url: 'http://www.citycantina.sk/prevadzka/1', module: 'itb' },
 	{ id: 4, name: 'Alfa', url: 'http://restauracie.sme.sk/restauracia/restauracia-alfa_2512-petrzalka_664/denne-menu', module: 'alfa' },
@@ -11,7 +11,7 @@ module.exports = {
 	{ id: 6, name: 'Pizza Pazza', url: 'http://pizzapazza.sk/?page_id=645', module: 'pizzapazza', post: 'prevadzka2=1' },
 	{ id: 7, name: 'Kamenica - Corleone Pizza', url: 'http://www.pizzacorleone.sk/obedove-menu.html', module: 'kamenica' },
 	{ id: 8, name: 'Numero Uno', url: 'http://www.numerouno.sk/denne-menu/', module: 'numerouno' },
-	{ id: 9, name: 'Engerau restaurant', url: 'https://www.zomato.com/sk/bratislava/engerau-restaurant-petr%C5%BEalka-bratislava-v/menu#tabtop', module: 'engerau' },
+	{ id: 9, name: 'Engerau restaurant', url: 'https://www.zomato.com/sk/bratislava/engerau-restaurant-petr%C5%BEalka-bratislava-v/denn%C3%A9-menu', module: 'engerau' },
 	{ id: 10, name: 'Lokálka', url: 'http://www.lokalka.sk/kopcianska-2/', module: 'lokalka' }
 	],
 	themes: {
