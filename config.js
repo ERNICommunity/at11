6b698978652a1a4819c4b204280470f34d6eb510
yohny <jan.nescivera@gmail.com>
2017-03-31 16:20:50 +0200

    removed unused zomatoId, added Kasa, resolves #119

--- config.js
+++ config.js
@@ -3,16 +3,17 @@ module.exports = {
 	cacheExpiration: 2 * 60 * 60 * 1000, //2h
 	parserTimeout: 10 * 1000, //10s
 	restaurants: [
-	{ id: 1, name: 'U Daňováka', zomatoId: '16507932', url: 'http://www.udanovaka.sk/obedove-menu/', module: 'danovak' },
+	{ id: 1, name: 'U Daňováka', url: 'http://www.udanovaka.sk/obedove-menu/', module: 'danovak' },
 	{ id: 2, name: 'Giuliano', url: 'http://www.giuliano.sk/-denne-menu', module: 'giuliano' },
 	{ id: 3, name: 'ITB', url: 'http://www.citycantina.sk/prevadzka/1', module: 'itb' },
-	{ id: 4, name: 'Alfa', zomatoId: '16507678', url: 'http://restauracie.sme.sk/restauracia/restauracia-alfa_2512-petrzalka_664/denne-menu', module: 'alfa' },
+	{ id: 4, name: 'Alfa', url: 'http://restauracie.sme.sk/restauracia/restauracia-alfa_2512-petrzalka_664/denne-menu', module: 'alfa' },
 	{ id: 5, name: 'Škôlka', url: 'http://jedalen.vysnivany.sk/ukazka-strany', module: 'skolka' },
 	{ id: 6, name: 'Pizza Pazza', url: 'http://pizzapazza.sk/?page_id=645', module: 'pizzapazza', post: 'prevadzka2=1' },
-	{ id: 7, name: 'Kamenica - Corleone Pizza', zomatoId: '16510390', url: 'http://www.pizzacorleone.sk/obedove-menu.html', module: 'kamenica' },
+	{ id: 7, name: 'Kamenica - Corleone Pizza', url: 'http://www.pizzacorleone.sk/obedove-menu.html', module: 'kamenica' },
 	{ id: 8, name: 'Numero Uno', url: 'http://www.numerouno.sk/denne-menu/', module: 'numerouno' },
-	{ id: 9, name: 'Engerau restaurant', zomatoId: '16507706', url: 'https://www.zomato.com/sk/bratislava/engerau-restaurant-petr%C5%BEalka-bratislava-v/denn%C3%A9-menu', module: 'engerau' },
-	{ id: 10, name: 'Lokálka', url: 'http://www.lokalka.sk/kopcianska-2/', module: 'lokalka' }
+	{ id: 9, name: 'Engerau restaurant', url: 'https://www.zomato.com/sk/bratislava/engerau-restaurant-petr%C5%BEalka-bratislava-v/denn%C3%A9-menu', module: 'engerau' },
+	{ id: 10, name: 'Lokálka', url: 'http://www.lokalka.sk/kopcianska-2/', module: 'lokalka' },
+	{ id: 11, name: 'Kaša', url: 'https://restauracie.sme.sk/restauracia/kasa-2_8386-petrzalka_664/denne-menu', module: 'kasa' }
 	],
 	themes: {
 		'jano': { name: 'Jano', template: '../views/index.html' },
