e6adb740d4b6fa07312b9816b706f14012b2ead3
milos.karas <milos.karas@erni.sk>
2018-01-19 10:06:28 +0100

    tests fix

--- parsers/pizzapazza.js
+++ parsers/pizzapazza.js
@@ -3,7 +3,7 @@ var parserUtil = require('./parserUtil');
 
 module.exports.parse = function(html, date, callback) {
     var $ = cheerio.load(html);
-    var dayMenu = [];  
+    var dayMenu = [];
 
     var menuItems = $("form.productDetail");
 	menuItems.each(function(){
@@ -21,12 +21,12 @@ module.exports.parse = function(html, date, callback) {
     callback(dayMenu);
 
 	function parseItem(item){
-		var itemParent = item.parent()
+		var itemParent = item.parent();
 		var name = itemParent.find("h2.MainTitle").text();
 		if (name){
 			var nameParts = name.split("   ");
 			var food = normalize(nameParts[0]);
-			var foodPrice = parserUtil.parsePrice(itemParent.find("div.product_price").text()).price;	
+			var foodPrice = parserUtil.parsePrice(itemParent.find("div.product_price").text()).price;
 			var soup = nameParts[1];
 			
 			dayMenu.push({
@@ -46,12 +46,12 @@ module.exports.parse = function(html, date, callback) {
 	}
 	
 	function dayMenuContainsSoup(soup){
-		return dayMenu.filter(function(e) {e.text === soup}).length > 0;
+		return dayMenu.filter(function(e) {return e.text === soup;}).length > 0;
 	}
 	
 	function normalize(str) {
         return str.removeItemNumbering()
-			.replace("/"," ")
+			.replace("/", " ")
             .normalizeWhitespace()
             .removeMetrics()
             .correctCommaSpacing();
--- test/pizzapazza.js
+++ test/pizzapazza.js
@@ -8,11 +8,11 @@ moment.locale('sk');
 describe('pizzapazza', function() {
     describe('parsing sample 2015-08-01', function() {
 
-        var html = fs.readFileSync(__dirname + '/samples/PizzaPazza.2015-08-01.html', { encoding: "utf-8" });
+        var html = fs.readFileSync(__dirname + '/samples/PizzaPazza.2018-01-19.html', { encoding: "utf-8" });
         var menu;
 
         before(function(done) {
-            parser.parse(html, moment("2015-08-03"), function(menuItems) {
+            parser.parse(html, moment("2018-01-19"), function(menuItems) {
                 menu = menuItems;
                 done();
             });
@@ -24,20 +24,20 @@ describe('pizzapazza', function() {
 
         it("1st item correct", function() {
             assert.equal(menu[0].isSoup, true);
-            assert.equal(menu[0].text, "Talianska cícerová");
+            assert.equal(menu[0].text, "Rascová s vajíčkom");
             assert.equal(isNaN(menu[0].price), true);
         });
 
         it("2nd item correct", function() {
             assert.equal(menu[1].isSoup, false);
-            assert.equal(menu[1].text.trim(), "Penne Polo e Funghi - cestoviny so šampiňónmi, kuracím mäsom a parmezánom");
-            assert.equal(menu[1].price, 4.00);
+            assert.equal(menu[1].text.trim(), "Hovädzí Stroganov, domáce halušky");
+            assert.equal(menu[1].price, 4.80);
         });
 
         it("3rd item correct", function() {
             assert.equal(menu[2].isSoup, false);
-            assert.equal(menu[2].text, "Obrátený bravčový rezeň, ryža");
-            assert.equal(menu[2].price, 4.80);
+            assert.equal(menu[2].text, "Vyprážané rybie filé, slovenský zemiakový šalát");
+            assert.equal(menu[2].price, 4.30);
         });
     });
 });
