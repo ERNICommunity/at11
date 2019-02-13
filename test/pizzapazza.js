3470c44555ecde9b907bb00e1b7696e08b88f988
jakub-sturc <jakub.sturc@gmail.com>
2019-02-13 11:56:13 +0100

    Fix pizza pazza tests
    Order of menu items was wrong

--- test/pizzapazza.js
+++ test/pizzapazza.js
@@ -6,7 +6,7 @@
 moment.locale('sk');
 
 describe('pizzapazza', function() {
-    describe('parsing sample 2015-08-01', function() {
+    describe('parsing sample 2018-01-19', function() {
 
         var html = fs.readFileSync(__dirname + '/samples/PizzaPazza.2018-01-19.html', { encoding: "utf-8" });
         var menu;
@@ -23,21 +23,21 @@ describe('pizzapazza', function() {
         });
 
         it("1st item correct", function() {
-            assert.equal(menu[0].isSoup, true);
+            assert.equal(menu[0].isSoup, true, "It should be soup.");
             assert.equal(menu[0].text, "Rascová s vajíčkom");
-            assert.equal(isNaN(menu[0].price), true);
+            assert.equal(isNaN(menu[0].price), true, "It should not have price");
+        });        
+
+        it("2rd item correct", function() {
+            assert.equal(menu[1].isSoup, false, "It should not be soup.");
+            assert.equal(menu[1].text, "Vyprážané rybie filé, slovenský zemiakový šalát");
+            assert.equal(menu[1].price, 4.30);
         });
 
-        it("2nd item correct", function() {
-            assert.equal(menu[1].isSoup, false);
-            assert.equal(menu[1].text.trim(), "Hovädzí Stroganov, domáce halušky");
-            assert.equal(menu[1].price, 4.80);
-        });
-
-        it("3rd item correct", function() {
-            assert.equal(menu[2].isSoup, false);
-            assert.equal(menu[2].text, "Vyprážané rybie filé, slovenský zemiakový šalát");
-            assert.equal(menu[2].price, 4.30);
+        it("3nd item correct", function() {
+            assert.equal(menu[2].isSoup, false, "It should not be soup.");
+            assert.equal(menu[2].text.trim(), "Hovädzí Stroganov, domáce halušky");
+            assert.equal(menu[2].price, 4.80);
         });
     });
 });
