cd2a65a50d84c12bdfc6fff73fd1f28dd1173360
Ján Neščivera <jan.nescivera@gmail.com>
2020-01-20 19:57:41 +0100

    better soup handling in tiffany

--- parsers/tiffany.ts
+++ parsers/tiffany.ts
@@ -9,7 +9,7 @@ export class Tiffany extends Zomato implements IParser {
     public parse(html: string, date: Moment, doneCallback: (menu: IMenuItem[]) => void): void {
         const menuItems = super.parseBase(html, date);
 
-        const soups = menuItems[0].text.replace(/^\s*[Pp]oli?evk[ay]\s*:\s*-\s*/, "").replace(/\s*\(0[.,]25\)\s*$/, "").split(",");
+        const soups = menuItems[0].text.replace(/^\s*[Pp]oli?evk[ay]\s*:\s*-\s*/, "").replace(/\s*\(0[.,]25l?\)\s*$/, "").split(",");
         menuItems.shift();
         for (const soup of soups) {
           menuItems.unshift({ isSoup: true, text: soup, price: NaN });
